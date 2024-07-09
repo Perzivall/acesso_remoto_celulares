90d28679019b11554c6ef468a44fdccf39628ba3
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-07-09 14:28:19 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 clear
 echo 'Iniciando instalador...'
+read -p 'Digite o IP do Wireguard do lado do CLiente: ' IP_ADDR
 sleep 3
 echo ''
 echo 'termux-wake-lock' >> ~/.bashrc
 echo 'sshd' >> ~/.bashrc
 echo 'export DISPLAY=":1"' >> ~/.bashrc
-pkg install openssh xfce4 firefox -y 
-read -p 'Digite o IP do Wireguard do lado do CLiente' IP_ADDR
+pkg install openssh xfce4 tigervnc firefox -y 
 vncserver -from $IP_ADDR :1
 echo 'xfce4-session' >> ~/.vnc/xstartup
 echo 'Change root password now...'
