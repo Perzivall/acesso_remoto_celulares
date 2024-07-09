63896bd9031986acf76c5919795654ec0b1ea5b1
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-07-09 13:52:04 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ echo 'termux-wake-lock' >> ~/.bashrc
 echo 'sshd' >> ~/.bashrc
 echo 'export DISPLAY=":1"' >> ~/.bashrc
 source ~/.bashrc
-pkg update -y && pkg upgrade &&
-pkg install openssh xfce4 firefox -y &&
+pkg update -y && pkg upgrade 
+pkg install openssh xfce4 firefox -y 
 passwd
 read -p 'Digite o IP do Wireguard do lado do CLiente' IP_ADDR
 vncserver -from $IP_ADDR :1
 echo 'xfce4-session' >> ~/.vnc/xstartup
+echo 'Change root password now...'
+passwd
 exit
