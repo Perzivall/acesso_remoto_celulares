15649a55425fac3f522ffa8cc7cdb69a5ce72a2f
Perzivall <luciano.thiago.antunes@gmail.com>
2024-07-09 13:45:44 -0300

    Commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo 'Iniciando instalador...'
+sleep 3
+echo ''
+echo 'termux-wake-lock' >> ~/.bashrc
+echo 'sshd' >> ~/.bashrc
+echo 'export DISPLAY=":1"' >> ~/.bashrc
+source ~/.bashrc
+pkg update -y && pkg upgrade &&
+pkg install openssh xfce4 firefox -y &&
+passwd
+read -p 'Digite o IP do Wireguard do lado do CLiente' IP_ADDR
+vncserver -from $IP_ADDR :1
+echo 'xfce4-session' >> ~/.vnc/xstartup
+exit
